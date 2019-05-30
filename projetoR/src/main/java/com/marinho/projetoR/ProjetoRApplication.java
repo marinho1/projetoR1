5f598278761bb27b3d66b0749342e05529676329
marinho1 <netamsal@gmail.com>
2019-05-30 14:35:50 -0300

    Initial commit

---
+++ projetoR/src/main/java/com/marinho/projetoR/ProjetoRApplication.java
@@ -0,0 +1,13 @@
+package com.marinho.projetoR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoRApplication.class, args);
+	}
+
+}
---
+++ projetoR/src/test/java/com/marinho/projetoR/ProjetoRApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marinho.projetoR;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetoRApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
